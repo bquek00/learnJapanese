5f9be46644e22306b52c1a03c5812517134bdbe0
Bryan Quek <bryanquek@d-i89-199-167.student.eduroam.uq.edu.au>
2023-09-21 13:21:02 +1000

    Added basic testing page

---
+++ app/test/testCard.jsx
@@ -0,0 +1,55 @@
+"use client";
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+export default function Cards({word, data, id, count}) {
+    const [options, setOptions] = useState([]);
+    const[name, setName] = useState(word.slug) 
+    const[selected, setSelected] = useState(null) 
+
+    const handleClick = (answer) => {
+      setSelected(answer)
+    };
+
+    useEffect(() => {
+        if (data && Array.isArray(data)) {
+            // Make a copy of the data array using the spread operator
+            const dataCopy = [...data];
+    
+            let numItems = (data.length >= 4) ? 3 : data.length;
+
+            const slicedData = dataCopy.slice(0, numItems)
+
+            slicedData.push(word)
+            
+            // Shuffle the copied array and pick the first 'count' number of elements
+            const shuffledData = slicedData.sort(() => 0.5 - Math.random());
+
+            setOptions(shuffledData);
+            setName(word.slug)
+        }
+        }, [data, count]);
+    
+
+    return (
+        <div className='bg-white m-3 p-2 rounded-xl'>
+            <p className='text-2xl'>Match the word with the definition</p>
+            <p className='text-3xl'>{name}</p>
+
+            <p className='text-2xl'>Options</p>
+
+            <div className='flex flex-col'>
+                {options && options.map((item, index) => (
+                <button key={index} 
+                className={`text-start m-1 p-1 ${(selected && selected.id === item.id)? "bg-gray-200": "bg-gray-100 hover:bg-gray-200"} 
+                focus:ring-gray-300 rounded-lg`}
+                onClick={() => handleClick(item)}>
+                    {item.jishodata.senses[0].english_definitions.join(", ")}
+                </button>
+                ))}
+            </div>
+
+        </div>
+	);
+}
---
+++ app/test/testSection.jsx
@@ -0,0 +1,35 @@
+"use client";
+import { useState } from 'react';
+import Loader from '@/components/Loader';
+import { AppContext } from '@/context/NavContext';
+import { createContext, useContext} from 'react';
+import { useEffect } from 'react';
+import Link from 'next/link';
+import Cards from './testCard';
+
+export default function TestSection({data, count})  {
+     // State to store the selected random data
+     const [randomData, setRandomData] = useState([]);
+
+     useEffect(() => {
+        if (data && Array.isArray(data)) {
+            // Make a copy of the data array using the spread operator
+            const dataCopy = [...data];
+            
+            // Shuffle the copied array and pick the first 'count' number of elements
+            const shuffledData = dataCopy.sort(() => 0.5 - Math.random());
+            setRandomData(shuffledData.slice(0, count));
+        }
+        }, [data, count]);
+
+    return(
+        <div>   
+             {randomData && randomData.map((item, index) => (
+                <Cards key={index} word={item} 
+                data={data.filter((cur, filterIndex) => cur.id !== item.id)}  count={count}/>
+            ))}
+        </div>
+        
+    )
+}
+
